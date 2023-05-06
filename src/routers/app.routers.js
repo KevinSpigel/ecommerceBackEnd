fc174cba1c8b8efc33d1c053d5ec37e7e7bbb564
KevinSpigel <kevinspigel@gmail.com>
2023-05-06 08:59:07 +0200

    delete loggerTest route and controller

--- src/routers/app.routers.js
+++ src/routers/app.routers.js
@@ -5,7 +5,6 @@ const cartsRoutes = require("./carts/carts.routes");
 const sessionsRoutes = require("./sessions/sessions.routes");
 const mocksProductsRoutes = require("./mocks/mocksProducts.routes");
 const usersRoutes = require("./users/users.routes");
-const loggersTestRoutes = require("./loggers/loggersTest.routes");
 
 const router = Router();
 
@@ -14,7 +13,6 @@ router.use("/carts", cartsRoutes.getRouter());
 router.use("/sessions", sessionsRoutes);
 router.use("/users", usersRoutes.getRouter());
 router.use("/mockingproducts", mocksProductsRoutes.getRouter());
-router.use("/loggerTest", loggersTestRoutes.getRouter());
 
 router.use(errorMiddleware);
 
