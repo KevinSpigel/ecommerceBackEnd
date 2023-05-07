5c6597f4ce59e5bf4692ded8d2f31a6c8195fdcf
KevinSpigel <kevinspigel@gmail.com>
2023-05-07 21:35:27 +0200

    replace clusterization with docker and kubernetes methods

--- src/server.js
+++ src/server.js
@@ -1,17 +1,42 @@
+const env = require("./config/env.config");
 const express = require("express");
 const path = require("path");
 const handlebars = require("express-handlebars");
 require("./config/db.config");
+const socketServer = require("./socket/socket.controller");
 
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 
+const { logger } = require("./logger/logger");
 const addLogger = require("./middlewares/logger.middleware");
 
 const viewsRoutes = require("./routers/views/views.routes");
 const apiRoutes = require("./routers/app.routers");
 
 const app = express();
+const PORT = env.PORT;
+
+// Listen
+const httpServer = app.listen(PORT, () => {
+  console.log(
+    `[${process.pid}] - The Server is up and running on port ${
+      httpServer.address().port
+    }`
+  );
+});
+
+// Server listen connection error
+httpServer.on("error", (error) => {
+  logger.fatal(
+    `There was an error trying to start the server on ${
+      httpServer.address().port
+    }`
+  );
+});
+
+// Socket
+socketServer(app, httpServer);
 
 // Template Engine
 app.engine("handlebars", handlebars.engine());
@@ -30,4 +55,14 @@ app.use(addLogger);
 app.use(viewsRoutes);
 app.use("/api", apiRoutes);
 
-module.exports = { app };
+// Add error handling for uncaught exceptions
+process.on("uncaughtException", (error) => {
+  logger.error(`Uncaught exception: ${error.message}`);
+  process.exit(1);
+});
+
+// Add error handling for unhandled promise rejections
+process.on("unhandledRejection", (error) => {
+  logger.error(`Unhandled promise rejection: ${error.message}`);
+  process.exit(1);
+});
